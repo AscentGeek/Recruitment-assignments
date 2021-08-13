3feecbaba750bc52367e6cbcc3d2e0e4c481d941
dgy66 <dgy66>
2021-08-13 20:06:34 +0900

    project design

---
+++ src/components/ProjectView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    project
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SideView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    side
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideView"
+}
+</script>
+
+<style scoped>
+
+</style>
