ac9a545c92f0c6b7d8e0bbaea5b53c4f36e8f0fe
dgy66 <dgy66>
2021-08-17 03:32:20 +0900

    project (view, add) pages with store actions

--- src/components/AddProjectView.vue
+++ src/components/AddProjectView.vue
@@ -5,8 +5,8 @@
       <div class="header">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li><button type="button" class="btn btn-default col-auto" @click="$router.push('/projects')">취소</button></li>
-            <li><button type="button" class="btn btn-primary col-auto">저장</button></li>
+            <li><button type="button" class="btn btn-default col-auto" @click="out">취소</button></li>
+            <li><button type="button" class="btn btn-primary col-auto" @click="addProject">저장</button></li>
           </ul>
         </nav>
         <a class="navbar-brand">프로젝트 관리</a>
@@ -20,13 +20,14 @@
           <div class="form-group col-md-2">
             <label>고객사명 <span style='color:orange;'>*</span></label>
             <select v-model="project.comp" class="form-select">
-              <option>고객사 선택</option>
-              <option>사용</option>
+              <option disabled value="">고객사 선택</option>
+              <option>신한</option>
+              <option>KB</option>
             </select>
           </div>
           <div class="form-group col-md-2">
             <label>프로젝트명 <span style='color:orange;'>*</span></label>
-            <input v-model="project.name" class="form-control" placeholder="프로젝트명 입력" />
+            <input v-model="project.name" class="form-control" placeholder="프로젝트명 입력" maxlength="100"/>
           </div>
         </div>
       </form>
@@ -38,18 +39,18 @@
             <label>사용여부 <span style='color:orange;'>*</span></label>
             <div>
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" id="new" value="new" v-model="project.used">
+                <input class="form-check-input" type="radio" id="new" value="미사용" v-model="project.used" checked="checked"/>
                 <label class="form-check-label" for="new">미사용</label>
               </div>
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" id="used" value="used" v-model="project.used">
+                <input class="form-check-input" type="radio" id="used" value="사용" v-model="project.used"/>
                 <label class="form-check-label" for="used">사용</label>
               </div>
             </div>
           </div>
           <div class="form-group col-md-2">
             <label>기간 <span style='color:orange;'>*</span></label>
-            <input type="date" v-model="project.date" class="form-control" placeholder="기간 입력" />
+            <input type="date" v-model="project.dRange" class="form-control" placeholder="기간 입력" />
           </div>
         </div>
       </form>
@@ -60,21 +61,31 @@
           <div class="form-group col-md-2">
             <label>산업구분 <span style='color:orange;'>*</span></label>
             <select v-model="project.cate" class="form-select">
-              <option>산업구분 선택</option>
+              <option disabled value="">산업구분 선택</option>
               <option>코드관리</option>
             </select>
           </div>
           <div class="form-group col-md-2">
             <label>브랜드 <span style='color:orange;'>*</span></label>
-            <input v-model="project.brand" class="form-control" placeholder="브랜드 입력" />
+            <input v-model="project.brand" class="form-control" placeholder="브랜드 입력" maxlength="100"/>
           </div>
           <div class="form-group col-md-2">
             <label>브랜드URL</label>
-            <input v-model="project.bURL" class="form-control" placeholder="브랜드 URL 입력" />
+            <input v-model="project.bURL" class="form-control" placeholder="브랜드 URL 입력" maxlength="100"
+                    oninput=" this.value = this.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g, '' );" />
           </div>
         </div>
       </form>
     </div>
+
+    <div class="row">
+      <div>
+        <h5 class="pull-left">경쟁사</h5>
+        <button class="pull-right btn btn-none" @click="addRow">+ 경쟁사 추가</button>
+      </div>
+    </div>
+
+    <hr>
 
     <div class="row">
       <div class="table-responsive">
@@ -86,35 +97,162 @@
             </tr>
           </thead>
           <tbody>
-          <tr v-for="item in comp" :key="item.id">
-            <td><input type="checkbox"/></td>
-            <td>{{ item }}</td>
+          <tr v-for="item in enemy" :key="item.id">
+            <td><input type="checkbox" :value="item.id" @click="test"/></td>
+            <td><input v-model="item.name" type="text" class="form-control" id="ename" maxlength="100"/></td>
+            <td><input v-model="item.eURL" type="text" class="form-control" id="eURL" maxlength="100"
+                       oninput=" this.value = this.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g, '' );" /></td>
           </tr>
           </tbody>
         </table>
       </div>
     </div>
+
+    <Modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        경고!
+      </h3>
+      <div slot="body">
+        {{comment}}
+      </div>
+      <div slot="footer" v-if="btn==false">
+        <button class="btn btn-default" @click="showModal = false">NO</button>
+        <button class="btn btn-primary" @click="movePage">YES</button>
+      </div>
+      <div slot="footer" v-else>
+        <button class="btn btn-primary" @click="showModal = false">OK</button>
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal'
+
 export default {
   name: "AddProject",
+  components: { Modal },
   data(){
     return{
       comp: ["경쟁사명", "경쟁사URL"],
       theads: ["고객사명", "프로젝트명", "사용여부", "산업구분", "브랜드", "브랜드URL", "수정일시"],
-      option: 10,
-      items: [],
+      option: 5,
       project: {
-        comp: "고객사 선택",
+        id: this.$store.getters.createdProjects.length,
+        comp: "",
         name: "",
-        used: "",
-        date: "",
-        cate: "산업구분 선택",
+        used: "미사용",
+        date: Date.now(),
+        dRange: "",
+        cate: "",
         brand: "",
-        bURL: ""
+        bURL: "",
+      },
+      enemy: [
+        {
+          name: "",
+          eURL: ""
+        },
+        {
+          name: "",
+          eURL: ""
+        },
+        {
+          name: "",
+          eURL: ""
+        },
+        {
+          name: "",
+          eURL: ""
+        },
+        {
+          name: "",
+          eURL: ""
+        }
+      ],
+      showModal: false,
+      comment: "",
+      btn: true
+    }
+  },
+
+  methods:{
+    addProject(){
+      if(this.project.name != '' && this.project.comp != '' && this.project.cate != '' && this.project.brand != '' && this.project.cate != '' && this.project.used != '') {
+        const enemy = Array()
+        const project = this.project
+        for (let i in this.enemy){
+          if(this.enemy[i].name != "" && this.enemy[i].eURL != ""){
+            enemy.push({
+              name: this.enemy[i].name,
+              eURL: this.enemy[i].eURL
+            })
+          }
+        }
+        if (this.$route.params.item != undefined) {
+          this.$store.commit('updateProject', {project, enemy});
+        } else {
+          this.$store.commit('addProject', {project, enemy});
+        }
+        // this.$store.commit('addProjectProperties', this.enemy);
+        this.movePage();
+      } else {
+        this.comment = '필수 항목을 입력해주세요.'
+        this.btn = true;
+        this.Modal();
       }
+    },
+    movePage(){
+      this.$router.push('/projects');
+    },
+    addRow() {
+      if(this.option < 10) {
+        this.enemy.push({
+          name: "",
+          eURL: ""
+        })
+        this.option++
+      }
+    },
+    out(){
+      this.comment = '페이지를 벗어나시겠습니까?';
+      this.btn = false;
+      this.Modal();
+    },
+    Modal(){
+      this.showModal = !this.showModal;
+    },
+    test(){
+      // const a = this.project;
+      // const b = this.enemy;
+      const b = Array()
+      for (let i in this.enemy){
+        if(this.enemy[i].name != ""){
+          b.push({
+            name: this.enemy[i].name,
+            eURL: this.enemy[i].eURL
+          })
+        }
+      }
+      console.log(b)
+      // this.$store.commit('addProject', {a, b});
+    }
+  },
+
+  computed:{
+    // setRows(){
+    //   for(let i in this.option){
+    //     this.enemy[i].name = "";
+    //     this.enemy[i].eURL = "";
+    //   }
+    //   return this.enemy
+    // }
+  },
+
+  created(){
+    if(this.$route.params.item) {
+      this.project = this.$route.params.item
+      this.enemy = this.$route.params.item.enemy
     }
   }
 }
@@ -135,5 +273,16 @@ export default {
   }
   .btn-default{
     background-color: lightgray;
+  }
+  .btn-none{
+    color: gray;
+  }
+  h5{
+    margin: 5px;
+    font-size: 1.25rem;
+    font-weight: bold;
+  }
+  .table tr{
+    text-align: center;
   }
 </style>
--- src/components/ProjectView.vue
+++ src/components/ProjectView.vue
@@ -18,18 +18,18 @@
           </div>
           <div class="form-group col-md-2">
             <label for="project">프로젝트명</label>
-            <input v-model="search.project" type="text" class="form-control" id="project" placeholder="프로젝트명">
+            <input v-model="search.name" type="text" class="form-control" id="project" placeholder="프로젝트명">
           </div>
           <div class="form-group col-md-2">
             <label for="cate">산업구분</label>
-            <select v-model="search.used" class="form-select" id="cate">
-              <option>전체</option>
+            <select v-model="search.cate" class="form-select" id="cate">
+              <option value="">전체</option>
             </select>
           </div>
           <div class="form-group col-md-2">
             <label for="used">사용여부</label>
             <select v-model="search.used" class="form-select" id="used">
-              <option>전체</option>
+              <option value="">전체</option>
               <option>사용</option>
               <option>미사용</option>
             </select>
@@ -47,6 +47,7 @@
         <div class="form-row">
           <div class="form-group col-md-1">
             <select v-model="option" class="form-select">
+              <option>5</option>
               <option>10</option>
               <option>50</option>
               <option>100</option>
@@ -65,64 +66,95 @@
         <table class="table table-striped">
           <thead>
             <tr class="table-primary">
-              <th v-for="item in theads" :key="item.id">{{ item }}</th>
+              <th style="border-bottom: 1px solid black" v-for="item in theads" :key="item.id">{{ item }}</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item in items" :key="item.id">
-              <td>{{ item }}</td>
+            <tr v-for="(item, index) in filteredProjects" :key="index">
+              <td>{{ item.comp }}</td>
+              <td><router-link
+                  :to="{name: 'addproject', params: {item}, props: true}"
+                  tag="span"
+                  class="project_name">{{ item.name }}</router-link></td>
+              <td>{{ item.used }}</td>
+              <td>{{ item.cate }}</td>
+              <td>{{ item.brand }}</td>
+              <td>{{ item.bURL }}</td>
+              <td>{{ item.date }}</td>
+<!--              <td>{{ String(item.date).format('YYYY/MM/DD hh:mm:ss') }}</td>-->
             </tr>
           </tbody>
         </table>
-        <div class="row">
-          <nav>
-            <ul class="pagination justify-content-center">
-              <li>
-                <a href="#" aria-label="Previous">
-                  <span aria-hidden="true">&laquo;</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>&laquo;</span>
-                </a>
-              </li>
-              <li><a href="#">1</a></li>
-              <li><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-              <li><a href="#">4</a></li>
-              <li><a href="#">5</a></li>
-              <li>
-                <a href="#">
-                  <span>&raquo;</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" aria-label="Next">
-                  <span aria-hidden="true">&raquo;</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+<!--        <div class="row">-->
+<!--          <nav>-->
+<!--            <ul class="pagination justify-content-center">-->
+<!--              <li>-->
+<!--                <a href="#" aria-label="Previous">-->
+<!--                  <span aria-hidden="true">&laquo;</span>-->
+<!--                </a>-->
+<!--              </li>-->
+<!--              <li>-->
+<!--                <a href="#">-->
+<!--                  <span>&laquo;</span>-->
+<!--                </a>-->
+<!--              </li>-->
+<!--              <li><a href="#">1</a></li>-->
+<!--              <li><a href="#">2</a></li>-->
+<!--              <li><a href="#">3</a></li>-->
+<!--              <li><a href="#">4</a></li>-->
+<!--              <li><a href="#">5</a></li>-->
+<!--              <li>-->
+<!--                <a href="#">-->
+<!--                  <span>&raquo;</span>-->
+<!--                </a>-->
+<!--              </li>-->
+<!--              <li>-->
+<!--                <a href="#" aria-label="Next">-->
+<!--                  <span aria-hidden="true">&raquo;</span>-->
+<!--                </a>-->
+<!--              </li>-->
+<!--            </ul>-->
+<!--          </nav>-->
+<!--        </div>-->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
+
 export default {
   name: "ProjectView",
   data(){
     return{
       theads: ["고객사명", "프로젝트명", "사용여부", "산업구분", "브랜드", "브랜드URL", "수정일시"],
       option: 10,
-      items: [],
       search: {
         comp: "",
-        project: "",
-        cate: "전체",
-        used: "전체"
+        name: "",
+        cate: "",
+        used: ""
+      }
+    }
+  },
+
+  methods:{
+    test(){
+      console.log('test')
+    }
+  },
+
+  computed:{
+    ...mapGetters(['createdProjects']),
+
+    filteredProjects() {
+      if (this.search) {
+        return this.createdProjects.filter((item) => {
+          return item.name.startsWith(this.search.name);
+        })
+      } else {
+        return this.createdProjects;
       }
     }
   }
@@ -142,5 +174,21 @@ export default {
   .btn-default{
     margin-top: 20px;
     background-color: lightgray;
+  }
+  .project_name{
+    color: cornflowerblue;
+    text-decoration: underline cornflowerblue;
+  }
+  .table{
+    text-align: center;
+  }
+  .table-striped th{
+    background-color: #4285f2;
+  }
+  .table-striped tr:nth-child(odd) {
+    background-color: #ced6e6;
+  }
+  .table-striped tr:nth-child(even) {
+    background-color: #e7ebf2;
   }
 </style>
